7b100af1d2e35262b0f62a03237c9f14ff1dcb3a
ishaan.desai <ishaandesai@live.com>
2017-07-18 15:22:16 -0700

    Commit Cookie thing

--- src/Shop_Icon.java
+++ src/Shop_Icon.java
@@ -14,7 +14,7 @@ public class Shop_Icon extends GameObject {
     public void update(float dt){
         SpriteText st;
         if(InputManager.isMouseButtonReleased(0)){
-            System.out.println(InputManager.getMousePosition());
+            //System.out.println(InputManager.getMousePosition());
             if(InputManager.getMousePosition().getX() < 780 &&InputManager.getMousePosition().getX() > 741 &&InputManager.getMousePosition().getY() < 79&&InputManager.getMousePosition().getY() > 40){
                 GameObject sickbackrounddude = new Shop_Backround();
                 st = new SpriteText("The Shop", 32, SpriteText.Alignment.CENTER, 20, 0, 250);
