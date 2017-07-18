73d8e2dd64394fab8661ef8018f38a63475bc8ef
MarkyTheMan <1marcusyam1@gmail.com>
2017-07-18 10:26:43 -0700

    Initial Commit YayThisBeCoolsadw

---
+++ src/ClickPower.java
@@ -0,0 +1,19 @@
+import edu.digipen.gameobject.GameObject;
+
+/**
+ * Created by marcus.yim on 7/18/2017.
+ */
+public class ClickPower extends GameObject{
+
+    public ClickPower()  {
+        super("ClickPower", 200, 200, "Shop_Icon.png");
+    }
+
+    @Override
+    public void update(float dt){
+
+    }
+
+
+
+}
