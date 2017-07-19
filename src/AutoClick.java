cfcc0c497baba95750c9d4c0b3fe31381a510622
MarkyTheMan <1marcusyam1@gmail.com>
2017-07-19 13:34:00 -0700

    Initial Commit sssssssssssssssssssssssssssss

--- src/AutoClick.java
+++ src/AutoClick.java
@@ -1,4 +1,6 @@
+import edu.digipen.InputManager;
 import edu.digipen.gameobject.GameObject;
+import edu.digipen.gameobject.ObjectManager;
 
 /**
  * Created by marcus.yim on 7/18/2017.
@@ -9,9 +11,26 @@ public class AutoClick extends GameObject{
         super("AutoClick", 50, 50, "PlusCursor.png");
     }
 
+    boolean debounce = true;
+
     @Override
     public void update(float dt){
 
+
+        if(CookieButton.shop == true) {
+            if (InputManager.getMousePosition().getX() < 105 && InputManager.getMousePosition().getX() > 55 && InputManager.getMousePosition().getY() < 160 && InputManager.getMousePosition().getY() > 110) {
+                if (InputManager.isMouseButtonPressed(0) && debounce) {
+                    debounce = false;
+                    CookieButton cookieButton = (CookieButton) ObjectManager.getGameObjectByName("CookieButton");
+                    Level.auto++;
+                }
+                if (InputManager.isMouseButtonReleased(0)) {
+                    debounce = true;
+                }
+            } else {
+                debounce = true;
+            }
+        }
     }
 
 }
--- src/ClickPower.java
+++ src/ClickPower.java
@@ -11,16 +11,23 @@ public class ClickPower extends GameObject                                      
         super("ClickPower", 50, 50, "PlusUno2.png");
     }
 
+
     boolean debounce = true;
 
     @Override
     public void update(float dt){
+
+
         if(CookieButton.shop == true) {
             if (InputManager.getMousePosition().getX() < 105 && InputManager.getMousePosition().getX() > 55 && InputManager.getMousePosition().getY() < 160 && InputManager.getMousePosition().getY() > 110) {
                 if (InputManager.isMouseButtonPressed(0) && debounce) {
                     debounce = false;
                     CookieButton cookieButton = (CookieButton) ObjectManager.getGameObjectByName("CookieButton");
                    Level.power++;
+                    setModulationColor(1f, 0f, 0f, 0.5f);
+
+                    setModulationColor(0f, 0f, 0f,0f);
+
                 }
                 if (InputManager.isMouseButtonReleased(0)) {
                     debounce = true;
@@ -30,4 +37,6 @@ public class ClickPower extends GameObject                                      
             }
         }
     }
+
+
 }
