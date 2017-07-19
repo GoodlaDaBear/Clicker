93ce06957aae6b2273a1b1cff4b125bbb538cb30
matthew.yamamoto <matthew.yamamoto.digipen.edu>
2017-07-19 14:15:07 -0700

    commit hi

---
+++ src/bettertap.java
@@ -0,0 +1,19 @@
+/**
+ * Created by matthew.yamamoto on 7/19/2017.
+ */
+import edu.digipen.InputManager;
+import edu.digipen.gameobject.GameObject;
+import edu.digipen.gameobject.ObjectManager;
+public class bettertap extends GameObject{
+    SpriteText st;
+
+    public bettertap() {
+super ("better taps",350,219,"bettertap.png");
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+}
