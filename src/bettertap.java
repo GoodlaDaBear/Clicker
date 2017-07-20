2e660b5060aebba917c735a496c318910cf204a4
matthew.yamamoto <matthew.yamamoto.digipen.edu>
2017-07-20 11:22:05 -0700

    commit hi

--- src/bettertap.java
+++ src/bettertap.java
@@ -6,11 +6,13 @@ import edu.digipen.gameobject.GameObject;
 import edu.digipen.gameobject.ObjectManager;
 public class bettertap extends GameObject {
     SpriteText st;
-    float powerupcounter2 = 120;
+    float powerupcounter2 = 1;
     boolean powerup2= false;
     float texttimer2=2;
     float boxcounter2=3;
     boolean isTextShowing = false;
+    float powerupaffect =7;
+
     public bettertap() {
         super("better taps", 350, 219, "bettertap.png");
 
@@ -26,26 +28,31 @@ public class bettertap extends GameObject {
         boxcounter2-=dt;
         if (boxcounter2<= 0){
             setOpacity(0);
+
         }
         powerupcounter2 -= dt;
         if (powerupcounter2 <= 0) {
             setOpacity(100);
-            powerup2 = true;
         }
 
-        if (powerup2 == true) {
+        powerupaffect -= dt;
+        if (powerupaffect<=0){
             if (InputManager.isMouseButtonReleased(0)) {
                 System.out.println(InputManager.getMousePosition());
                 if (InputManager.getMousePosition().getX() < 145.0 && InputManager.getMousePosition().getX() > 60.0 && InputManager.getMousePosition().getY() < 99.0 && InputManager.getMousePosition().getY() > 59.0) {
                     st = new SpriteText("YOU GET 5 TIMES THE CLICK POWER FOR 7 SECONDS", 15, SpriteText.Alignment.CENTER, 7, -200, 200);
-
+                    // make the current tap times 5.
+                    CookieButton.change = CookieButton.change *5;
                     isTextShowing = true;
                     texttimer2 = 2;
                     boxcounter2=0;
-                    powerupcounter2=120;
+                    powerupcounter2=60;
                 }
 
             }
         }
+
+
+
     }
 }
